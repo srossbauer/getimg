9474f94fb920931460c704b7a47a3ab9909d0a44
srossbauer <stephan.rossbauer@gmail.com>
2015-05-22 13:49:41 +0200

    getimg

---
+++ getimg.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# download images from text file containing urls to current working directory
+# url must containing an image file name behind the last /
+# e.g. http://www.website.com/header.jpg
+# written by
+# stephan.rossbauer@gmail.com
+
+import sys, os, urllib2
+
+
+def url2file(url):
+    # function to download image from url
+    filename = url.split('/')[-1]
+    try:
+        resp = urllib2.urlopen(url).read()
+        if(os.path.exists(filename)):
+            print "ERROR filename already exists. Skipping url: "+url
+        else:
+            f = open(filename, 'w')
+            f.write(resp)
+            f.close()
+    except urllib2.URLError, e:
+        print "ERROR("+str(e.code)+") for url "+url
+
+		
+		
+# get filename from first commandline argument
+workfile = str(sys.argv[1])
+
+# catch errors with the commandline argument
+try:	
+	f = open(workfile, 'r')
+	for line in f:
+		line = line.rstrip()
+		url2file(line)
+	f.close()
+except IOError as e:
+    print("({})".format(e))
